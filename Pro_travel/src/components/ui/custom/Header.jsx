8d6aa533f5381f3e9f3ff7b136f9cc0fea8feec0
AnshulMavi <anshulmavi176@gmail.com>
2024-10-09 01:16:49 +0530

    new logo

--- Pro_travel/src/components/ui/custom/Header.jsx
+++ Pro_travel/src/components/ui/custom/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 function Header() {
   return (
     <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-    <img src="../public/logo.svg" alt="logo" height={100} width={100} />
+    <img src="../public/Logo.jpg" alt="logo" height={70} width={70} />
     <div>
         <Button>Login</Button>
     </div> 
